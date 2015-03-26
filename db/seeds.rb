a8da43a9f01b03f0cffba84253692670312fa2e3
Ngo Dinh Nguyen <nguyenngodinh@outlook.com>
2015-03-26 13:55:38 +0700

    finish book model with listing all book

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,35 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-# Users
-User.create!(name:  "Example User",
+ActiveRecord::Base.transaction do
+  User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar")
 
-User.create!(name:  "Đặng Minh Dũng",
-             email: "dungdm93@example.com",
-             password:              "qwerty",
-             password_confirmation: "qwerty")
+  User.create!(name:  "Đặng Minh Dũng",
+               email: "dungdm93@example.com",
+               password:              "qwerty",
+               password_confirmation: "qwerty")
 
-# Below user is unactivated
-# It's used for test
-User.create!(name:  "abc xyz",
-             email: "abcxyz@gmail.com",
-             password:              "123456",
-             password_confirmation: "123456")
+  User.create!(name:  "abc xyz",
+               email: "abcxyz@gmail.com",
+               password:              "123456",
+               password_confirmation: "123456")
 
-18.times do
-  name     = Faker::Name.name
-  email    = Faker::Internet.email
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
+  18.times do
+    name     = Faker::Name.name
+    email    = Faker::Internet.email
+    password = "password"
+    User.create!(name:  name,
+                 email: email,
+                 password:              password,
+                 password_confirmation: password)
+  end
+  20.times do
+    isbn = Faker::Code.isbn
+    title = Faker::Name.title
+    edition = Faker::Number.digit
+    publish_date = Faker::Date.between 100.days.ago, Date.today
+    author = Faker::Name.name 
+    number_of_pages = Faker::Number.number 3
+    book = Book.create! isbn: isbn, title: title, edition: edition, 
+                        publish_date: publish_date, author: author,
+                        number_of_pages: number_of_pages
+  end
 end
