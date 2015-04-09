af9c7cab04a61fb47f422c235e8f15c23c2fe568
dungdm93 <dungdm93@gmail.com>
2015-04-09 20:44:06 +0700

    Upload book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
-  
+  respond_to :html, :xml, :json
+
   def index
     @books = Book.search_by_title params[:title_query]
     if params.has_key?(:category_query) && params[:category_query].to_s != "all"
@@ -11,4 +12,19 @@ class BooksController < ApplicationController
     @book = Book.find params[:id]
   end
 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new book_params
+    @book.document = params[:book][:document]
+    flash[:notice] = 'Upload new book successfully.' if @book.save
+    respond_with @book
+  end
+
+  private
+  def book_params
+    params.require(:book).permit :isbn, :title, :edition, :author, :category_id
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,15 @@
 class Book < ActiveRecord::Base
-  validates :category_id, presence: true
   belongs_to :category
+  has_attached_file :document, styles: {thumb: ["595x842>", :png]},
+                    url:  "/uploads/:class/:attachment/:id_:style.:extension",
+                    path: ":rails_root/public:url"
+
+  validates :category_id, presence: true
+  validates_attachment :document,
+        presence: true,
+        size: {in: 0..100.megabytes}
+        # content_type: {content_type: "application/pdf"} # gem bug
+  do_not_validate_attachment_file_type :document
 
   scope :categories, -> category_id {where category_id: category_id}
 
@@ -11,5 +20,4 @@ class Book < ActiveRecord::Base
       Book.all
     end
   end
-
 end
